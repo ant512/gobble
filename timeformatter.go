94ecc33eba5efba53997d76da2aade02a64e8b3a
ant512 <ant@simianzombie.com>
2014-04-15 21:49:59 -0700

    Time formatting uses built-in functions.

--- timeformatter.go
+++ timeformatter.go
@@ -1,58 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+	"log"
 	"time"
 )
 
 func timeToFilename(t time.Time) string {
-	return fmt.Sprintf("%04d-%02d-%02d_%02d-%02d-%02d.md", t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second())
+	const layout = "2006-01-02_15-04-05.md"
+	return t.Format(layout)
 }
 
 func timeToString(t time.Time) string {
-	return fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d", t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second())
+	const layout = "2006-01-02 15:04:05"
+	return t.Format(layout)
 }
 
 func stringToTime(s string) time.Time {
-
-	year, err := strconv.Atoi(s[:4])
-
-	if err != nil {
-		return time.Unix(0, 0)
-	}
-
-	month, err := strconv.Atoi(s[5:7])
+	const layout = "2006-01-02 15:04:05"
+	t, err := time.Parse(layout, s)
 
 	if err != nil {
-		return time.Unix(0, 0)
+		log.Println(err)
 	}
 
-	day, err := strconv.Atoi(s[8:10])
-
-	if err != nil {
-		return time.Unix(0, 0)
-	}
-
-	hour, err := strconv.Atoi(s[11:13])
-
-	if err != nil {
-		return time.Unix(0, 0)
-	}
-
-	minute, err := strconv.Atoi(s[14:16])
-
-	if err != nil {
-		return time.Unix(0, 0)
-	}
-
-	seconds, err := strconv.Atoi(s[17:19])
-
-	if err != nil {
-		return time.Unix(0, 0)
-	}
-
-	location, err := time.LoadLocation("UTC")
-
-	return time.Date(year, time.Month(month), day, hour, minute, seconds, 0, location)
+	return t
 }
