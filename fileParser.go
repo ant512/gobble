eca32b96cfc22e1938bcaba515d6e0204180dcad
ant512 <ant@simianzombie.com>
2016-09-15 23:50:20 -0700

    Disabled hard line breaks in BlackFriday.

--- fileParser.go
+++ fileParser.go
@@ -65,7 +65,7 @@ func parseBlogFileHeader(text string, handler metadataParseHandler) int {
 
 func convertMarkdownToHtml(markdown *[]byte) string {
 	htmlFlags := blackfriday.HTML_USE_SMARTYPANTS
-	extensions := blackfriday.EXTENSION_AUTOLINK | blackfriday.EXTENSION_HARD_LINE_BREAK | blackfriday.EXTENSION_FENCED_CODE | blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	extensions := blackfriday.EXTENSION_AUTOLINK | blackfriday.EXTENSION_FENCED_CODE | blackfriday.EXTENSION_NO_INTRA_EMPHASIS
 
 	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
 
